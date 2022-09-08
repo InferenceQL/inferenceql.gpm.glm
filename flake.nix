801296d4a389b1e64c75080b4bce15e8aea2eeaf
Zane Shelby <zane@users.noreply.github.com>
2022-09-08 14:26:02 -0400

    build: Add flake.nix, flake.lock

---
+++ flake.nix
@@ -0,0 +1,23 @@
+{
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, nixpkgs-sppl, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+      in {
+        devShell = pkgs.mkShell {
+          buildInputs = with pkgs; [
+            clj-kondo
+            clojure
+            git
+            openblas
+            openjdk11
+          ];
+        };
+      }
+    );
+}
